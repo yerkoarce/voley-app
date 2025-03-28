717558b3106c51de53c9c0a1e4e958768aec9692
Yerko Arce <yerko.arce.galaz@gmail.com>
2025-03-28 19:09:39 -0300

    Se modifica y se agregan mensajes de aviso en el schema de zod

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -1,9 +1,9 @@
 const { z } = require("zod");
 
 const userSchema = z.object({
-  name: z.string().min(5).max(15),
-  username: z.string().min(5).max(15).regex(/^[a-zA-Z0-9_]+$/),
-  password: z.string().min(8).max(20),
+  name: z.string().min(5, {message: "Must be at least 5 characters"}).max(15, {message: "Must be at most 15 characters"}),
+  username: z.string().min(5, {message: "Must be at least 5 characters"}).max(15, {message: "Must be at most 15 characters"}).regex(/^[a-zA-Z0-9_]+$/, {message: "Username must contain only letters, numbers, and underscores"}),
+  password: z.string().min(8, {message: "Must be at least 8 characters"}).max(20, { message: "Must be at most 20 characters" }),
 });
 
 module.exports = userSchema;
