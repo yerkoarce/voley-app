cb7d846027902ac4aeb383982de391bb2498e591
yerko arce <yerko.arce.galaz@gmail.com>
2025-03-29 22:33:22 -0300

    Se modifica para hacer opcional el teamId

--- schemas/playerSchema.js
+++ schemas/playerSchema.js
@@ -4,7 +4,7 @@ const playerSchema = z.object({
   name: z.string().min(5, {message: "Must be at least 5 characters"}).max(15, {message: "Must be at most 15 characters"}),
   age: z.number().min(10, {message: "Must be at least 10 years old"}),
   position: z.string(),
-  teamId: z.string().uuid(),
+  teamId: z.string().uuid().optional(),
   userId: z.string().uuid().optional(),
   height: z.number().min(140, {message: "Must be at least 140 cm"}),
   attackReach: z.number().min(250, {message: "Must be at least 250 cm"}),
