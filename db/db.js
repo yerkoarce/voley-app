e426e78e7b1ad240a5f1b95ce7b26e425ffea953
yerko arce <yerko.arce.galaz@gmail.com>
2025-03-29 22:35:58 -0300

    Se borra la credencial del archivo para agregar al .env

--- db/db.js
+++ db/db.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose')
 const debugDB = require('debug')('voley-app:db')
 
-// mongo URI = "mongodb+srv://yerkoarcegalaz:1SZddpSYM6nEiQ15@voley-app.z5bc97a.mongodb.net/?retryWrites=true&w=majority&appName=voley-app"
-// Borrar despues esto y el commit
-
 const db = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI)
